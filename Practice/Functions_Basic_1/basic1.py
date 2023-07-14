90613d135217cfeaff34f2ac0f2184f37cc5e968
Fred452 <fredyarce452@gmail.com>
2023-07-14 15:28:12 -0400

    basic 1

---
+++ Practice/Functions_Basic_1/basic1.py
@@ -0,0 +1,152 @@
+# 1 - Return -> 5
+def número_de_grupos_alimentarios():
+    return 5
+print(número_de_grupos_alimentarios())
+
+
+#2
+def número_de_ramas_militares():
+    return 5
+# print(número_de_días_en_una_semana_silicona_o_lados_del_triángulo() + 
+# número_de_ramas_militares())
+
+
+#3 Return -> 5
+def número_de_libros_en_espera():
+    return 5
+    return 10
+print(número_de_libros_en_espera())
+
+
+#4 Return -> 5
+def número_de_dedos():
+    return 5
+    print(10)
+print(número_de_dedos())
+
+
+#5 Return ->  None
+def número_de_lagos_grandes():
+    print(5)
+x = número_de_lagos_grandes()
+print(x)
+
+
+#6 Return -> Error 
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+
+
+#7 Return -> Error 
+def concatenar(b,c):
+    return str(b)+str(c)
+x = "función de soporte python from-rainbow">print(concatenar(2,5))
+
+
+#8 Return -> 100 and 10
+def continentes():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(continentes())
+
+
+#9 Return -> 7, 14, 21
+def lados_de_triángulo(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(lados_de_triángulo(2,3))
+print(lados_de_triángulo(5,3))
+print(lados_de_triángulo(2,3) + lados_de_triángulo(5,3))
+
+
+#10 Return -» 8
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+
+
+#11 Print -> 500, 
+b = 500
+print(b)
+def foobar():
+    b ="operador de palabra clave from-rainbow">= 300 # -> that return error 
+    print(b)
+print(b)
+foobar()
+print(b)
+
+
+#12 Return -> 500, 500, 300, 300, 500
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+
+
+#13 Return -> 500, 500, 300, 300
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+
+
+#14 Print -> 1, 3, 2
+def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
+
+
+#15 Print -> 1, 3, 5, 10
+def foo():
+    print(1)
+    x = bar1()
+    print(x)
+    return 10
+def bar1():
+    print(3)
+    return 5
+y = foo()
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
